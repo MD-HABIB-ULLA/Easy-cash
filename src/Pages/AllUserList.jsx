f4677d0d92ac9878ae6640fb67f9a11b4366f490
habib ulla <habibulla1278@gmail.com>
2024-07-17 00:39:13 +0600

    all user route

--- src/Pages/AllUserList.jsx
+++ src/Pages/AllUserList.jsx
@@ -3,8 +3,8 @@
 const AllUserList = () => {
     return (
         <div>
-            <div>
-                <p className="text-5xl text-[#95D2B3]">all user</p>
+            <div className="min-h-screen bg-[#F1F8E8]">
+                <p className="text-5xl text-[#95D2B3] text-center font-semibold uppercase pt-5">all user</p>
             </div>
             
         </div>
